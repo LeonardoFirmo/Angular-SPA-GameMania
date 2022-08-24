b160b98769894fd9732b59a4fd76608cd9fd72cc
Leonardo Firmo <leonardofirmodamota@gmail.com>
2022-08-24 13:33:47 -0300

    Projeto SPA em Angular do site Game Mania

---
+++ src/app/views/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private loginService: LoginService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  
+  userModel = new User();
+
+  mensagem = ""
+
+  receberDados(){
+   console.log(this.userModel)
+
+    this.loginService.login(this.userModel).subscribe( (response) => {
+      console.log("Deu certo")
+      localStorage.setItem("nomeUsuario", response.body.user.nome)
+      this.router.navigateByUrl("/")
+      
+    },(respostaErro) =>{
+      console.log(respostaErro.error)
+      this.mensagem = respostaErro.error
+     
+      
+    })
+  }
+
+}
